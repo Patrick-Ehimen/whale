7fe23fb5f03687a0cf05151dcb1864d0503efb55
ayo <0xosepatrick@gmail.com>
2025-05-24 23:53:05 +0100

    delete asset

---
+++ public/index.ts
@@ -0,0 +1,29 @@
+import Logo from "./assets/logo.svg";
+import ChinaIcon from "./assets/china-icon.svg";
+import DutchIcon from "./assets/dutch-icon.svg";
+import UkIcon from "./assets/uk-icon.svg";
+import FranceIcon from "./assets/france-icon.svg";
+import SpainIcon from "./assets/spain-icon.svg";
+import SwedenIcon from "./assets/sweden-icon.svg";
+import MarginTrade from "./assets/margin-trade.svg";
+import MobileApp from "./assets/mobile-app.svg";
+import TradeApi from "./assets/trade-api.svg";
+import Wallet from "./assets/wallet.svg";
+import Widget from "./assets/widget.svg";
+import News from "./assets/news.svg";
+
+export {
+  Logo,
+  ChinaIcon,
+  DutchIcon,
+  UkIcon,
+  FranceIcon,
+  SpainIcon,
+  SwedenIcon,
+  MarginTrade,
+  MobileApp,
+  TradeApi,
+  Wallet,
+  Widget,
+  News,
+};
